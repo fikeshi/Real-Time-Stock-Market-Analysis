87a5fbb0aff611258232534683c0a2e4b1e2da14
Oluwafikunmi Eshilokun <efikunmi@yahoo.com>
2026-03-16 17:53:50 +0000

    integrate Spark into pipeline and implement ETL for Kafka streaming dat

---
+++ consumer/consumer.py
@@ -0,0 +1,58 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, StringType, TimestampType, FloatType
+from pyspark.sql.functions import from_json, col
+import os 
+
+#diirectory where spark will store its checkpoint data. crucial in streaming to enable fault tolerance
+checkpoint_dir = '/tmp/checkpoint/kafka_to_postgres'
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+
+
+#The schmea/struct matching the now data coming from Kafka
+kafka_data_schema = StructType([
+    StructField("date", StringType()),
+    StructField("high", StringType()),
+    StructField("low", StringType()),
+    StructField("open", StringType()),
+    StructField("close", StringType()),
+    StructField("symbol", StringType()) 
+])
+
+#initiaizing spark application with sparksession
+spark = (SparkSession.builder
+         .appName('KafkaSparkStreaming')
+         .getOrCreate())
+
+#reading from Kafka as a stream
+df = (spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "kafka:9092")
+    .option("subscribe", "stock_analysis")
+    .option("startingOffsets", "latest") #read only new incoming messages 
+    .option("failOnDataLoss", "false") #if kafka deletes old messages(retention), spark wont crash
+    .load() #start reading the kafka topic as a stream
+)
+
+parsed_df = df.selectExpr(
+    "CAST(value AS STRING)") \
+    .select(from_json(col("value"), kafka_data_schema).alias("data")).select("data.*")
+
+
+#casting and selecting final columns
+processed_df = parsed_df.select(
+    col("date").cast(TimestampType()).alias("date"),
+    col("high").alias("high"),
+    col("low").alias("low"),
+    col("open").alias("open"),
+    col("close").alias("close"),
+    col("symbol").alias("symbol"),
+)
+#Display results to the terminal (console output)
+query = processed_df.writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .option("truncate", "false") \
+    .option("checkpointLocation", checkpoint_dir) \
+    .start()
+#wait for a manual termination of the  pipeline
+query.awaitTermination()
