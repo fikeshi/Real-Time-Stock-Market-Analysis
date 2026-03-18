15c49e4a9b1902c7a2691b634e6b66bed35319f8
Oluwafikunmi Eshilokun <efikunmi@yahoo.com>
2026-03-18 21:08:25 +0000

    update on main

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -9,6 +9,15 @@ if not os.path.exists(checkpoint_dir):
     os.makedirs(checkpoint_dir)
 
 
+postgres_config ={
+    "url":"jdbc:postgresql://postgres:5432/stock_data",
+    "user":"admin",
+    "password":"admin",
+    "dbtable":"stocks",
+    "driver":"org.postgresql.Driver"
+}
+
+
 #The schmea/struct matching the now data coming from Kafka
 kafka_data_schema = StructType([
     StructField("date", StringType()),
@@ -48,11 +57,33 @@ processed_df = parsed_df.select(
     col("symbol").alias("symbol"),
 )
 #Display results to the terminal (console output)
-query = processed_df.writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .option("truncate", "false") \
-    .option("checkpointLocation", checkpoint_dir) \
+# query = processed_df.writeStream \
+#     .outputMode("append") \
+#     .format("console") \
+#     .option("truncate", "false") \
+#     .option("checkpointLocation", checkpoint_dir) \
+#     .start()
+
+
+def write_to_postgres(batch_df, batch_id):
+    """
+    Writes a microbatch dataframe to postgres using JDBC in 'append' mode
+    """
+    batch_df.write \
+        .format('jdbc') \
+        .mode('append') \
+        .options(**postgres_config) \
+        .save()
+
+#Stream Data to Postgres using foreachBatch
+query = (
+    processed_df.writeStream
+    .foreachBatch(write_to_postgres) #use foreachBatch for JDBC sinks
+    .option("checkpointLocation", checkpoint_dir) #directory where spark will store its
+    .OutputMode('append')
     .start()
+)
 #wait for a manual termination of the  pipeline
 query.awaitTermination()
+
+
